64a11fa932b89e10c49eff3206ac814e0b9f1a40
Stephen M. Pallen <smpallen99@yahoo.com>
2016-11-26 19:55:32 -0500

    updating router

--- web/router.ex
+++ web/router.ex
@@ -11,15 +11,6 @@ defmodule CoherenceDemo.Router do
     plug Coherence.Authentication.Session
   end
 
-  # pipeline :protected do
-  #   plug :accepts, ["html"]
-  #   plug :fetch_session
-  #   plug :fetch_flash
-  #   plug :protect_from_forgery
-  #   plug :put_secure_browser_headers
-  #   plug Coherence.Authentication.Session, protected: true
-  # end
-
   pipeline :api do
     plug :accepts, ["json"]
   end
@@ -28,15 +19,6 @@ defmodule CoherenceDemo.Router do
     pipe_through :browser
     coherence_routes :all
   end
-  # scope "/" do
-  #   pipe_through :protected
-  #   coherence_routes :protected
-  # end
-
-  # scope "/", CoherenceDemo do
-  #   pipe_through :browser
-  #   get "/", PageController, :index
-  # end
 
   scope "/", CoherenceDemo do
     pipe_through :browser
@@ -47,9 +29,4 @@ defmodule CoherenceDemo.Router do
     put "/unlock/:id", UserController, :unlock
     put "/confirm/:id", UserController, :confirm
   end
-
-  # Other scopes may use custom stacks.
-  # scope "/api", CoherenceDemo do
-  #   pipe_through :api
-  # end
 end
