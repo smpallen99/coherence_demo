3c1788961d0bf071cf520894919f7a37a720b17d
Sirius Dely <siriusdely@icloud.com>
2017-12-25 11:25:21 +0700

    'setup' improvement following @zwippie suggestion

--- test/coherence_demo_web/controllers/post_controller_test.exs
+++ test/coherence_demo_web/controllers/post_controller_test.exs
@@ -16,12 +16,18 @@ defmodule CoherenceDemoWeb.PostControllerTest do
     post
   end
 
-  setup [:create_user]
+  setup %{conn: conn} do
+    user = User.changeset(%User{}, @user_attrs)
+           |> Repo.insert!
+           |> Controller.confirm!
+    conn = conn
+           |> post(session_path(conn, :create), %{session: @user_attrs})
+    {:ok, conn: conn, user: user}
+  end
 
   describe "index" do
     test "lists all posts", %{conn: conn} do
       conn = conn
-             |> post(session_path(conn, :create), %{session: @user_attrs})
              |> get(post_path(conn, :index))
       assert html_response(conn, 200) =~ "Listing Posts"
     end
@@ -30,7 +36,6 @@ defmodule CoherenceDemoWeb.PostControllerTest do
   describe "new post" do
     test "renders form", %{conn: conn} do
       conn = conn
-             |> post(session_path(conn, :create), %{session: @user_attrs})
              |> get(post_path(conn, :new))
       assert html_response(conn, 200) =~ "New Post"
     end
@@ -39,21 +44,18 @@ defmodule CoherenceDemoWeb.PostControllerTest do
   describe "create post" do
     test "redirects to show when data is valid", %{conn: conn} do
       conn = conn
-             |> post(session_path(conn, :create), %{session: @user_attrs})
              |> post(post_path(conn, :create), post: @create_attrs)
 
       assert %{id: id} = redirected_params(conn)
       assert redirected_to(conn) == post_path(conn, :show, id)
 
       conn = conn
-             |> post(session_path(conn, :create), %{session: @user_attrs})
              |> get(post_path(conn, :show, id))
       assert html_response(conn, 200) =~ "Show Post"
     end
 
     test "renders errors when data is invalid", %{conn: conn} do
       conn = conn
-             |> post(session_path(conn, :create), %{session: @user_attrs})
              |> post(post_path(conn, :create), post: @invalid_attrs)
       assert html_response(conn, 200) =~ "New Post"
     end
@@ -64,7 +66,6 @@ defmodule CoherenceDemoWeb.PostControllerTest do
 
     test "renders form for editing chosen post", %{conn: conn, post: post} do
       conn = conn
-             |> post(session_path(conn, :create), %{session: @user_attrs})
              |> get(post_path(conn, :edit, post))
       assert html_response(conn, 200) =~ "Edit Post"
     end
@@ -75,19 +76,16 @@ defmodule CoherenceDemoWeb.PostControllerTest do
 
     test "redirects when data is valid", %{conn: conn, post: post} do
       conn = conn
-             |> post(session_path(conn, :create), %{session: @user_attrs})
              |> put(post_path(conn, :update, post), post: @update_attrs)
       assert redirected_to(conn) == post_path(conn, :show, post)
 
       conn = conn
-             |> post(session_path(conn, :create), %{session: @user_attrs})
              |> get(post_path(conn, :show, post))
       assert html_response(conn, 200) =~ "some updated body"
     end
 
     test "renders errors when data is invalid", %{conn: conn, post: post} do
       conn = conn
-             |> post(session_path(conn, :create), %{session: @user_attrs})
              |> put(post_path(conn, :update, post), post: @invalid_attrs)
       assert html_response(conn, 200) =~ "Edit Post"
     end
@@ -98,20 +96,12 @@ defmodule CoherenceDemoWeb.PostControllerTest do
 
     test "deletes chosen post", %{conn: conn, post: post} do
       conn = conn
-             |> post(session_path(conn, :create), %{session: @user_attrs})
              |> delete(post_path(conn, :delete, post))
       assert redirected_to(conn) == post_path(conn, :index)
       assert_error_sent 404, fn ->
         get conn, post_path(conn, :show, post)
       end
     end
-  end
-
-  defp create_user(_) do
-    {ok, user} = User.changeset(%User{}, @user_attrs)
-                  |> Repo.insert!
-                  |> Controller.confirm!
-    {ok, user: user}
   end
 
   defp create_post(_) do
