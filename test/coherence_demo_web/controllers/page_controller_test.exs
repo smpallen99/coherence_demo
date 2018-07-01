dfa7685e4f52b5ac564975fdec139e1e9a38a8b5
SocFoot <nzakou.cedric@gmail.com>
2018-07-01 14:36:49 +0200

    add build conn

--- test/coherence_demo_web/controllers/page_controller_test.exs
+++ test/coherence_demo_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule CoherenceDemoWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "An full-featured authentication package"
   end
 end
